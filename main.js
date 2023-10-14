b36d3aff069d9f34dcb669bfd3dab1a30a94f169
DCKAP <vigneshcdckap@gmail.com>
2023-10-14 10:05:45 +0530

    add

---
+++ main.js
@@ -0,0 +1,16 @@
+"user strict"
+{let a = 10;
+let b = 50;
+console.log(a+b);
+}
+{
+let a = "70";
+let b = 100;
+console.log(a+b);
+}
+var x = "80";
+var y = 90;
+console.log(x-y);
+
+var a = 100/5*5-10
+console.log(a)
